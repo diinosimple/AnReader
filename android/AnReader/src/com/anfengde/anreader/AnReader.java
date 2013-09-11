8e612cac7e336a8da428a4a453128f21b2320641
chtian <chtian@anfengde.com>
2013-09-11 23:09:56 +0800

    rename main activity

--- android/AnReader/src/com/anfengde/anreader/AnReader.java
+++ android/AnReader/src/com/anfengde/anreader/AnReader.java
@@ -13,68 +13,72 @@ import android.content.IntentFilter;
 import android.view.Menu;
 
 public class AnReader extends Activity {
-	private boolean foreground = false;
-	protected BroadcastReceiver broadcastReceiver = new BroadcastReceiver() {
-		@Override
-		public void onReceive(Context context, Intent intent) {
-			if (foreground)
-				return;
-			finish();
-		}
-	};
-	public BookView bookView;
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		foreground = true;
-		setContentView(R.layout.activity_an_reader);
-		BookView bookView = (BookView) findViewById(R.id.bookView); bookView.setPath(Constants.CACHE_PAHT); 
-		bookView.initBook();
-		bookView.openShelf();
-		Intent intent = getIntent();
-		bookView.openBookFromFileExplorer(intent);
-	}
+    private boolean foreground = false;
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.an_reader, menu);
-		return true;
-	}
+    protected BroadcastReceiver broadcastReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            if (foreground)
+                return;
+            finish();
+        }
+    };
 
-	@Override
-	public void onResume() {
-		super.onResume();
-		foreground = true;
-		IntentFilter filter = new IntentFilter();
-		filter.addAction("com.anfengde.ExitApp");
-		this.registerReceiver(this.broadcastReceiver, filter);
-		finishPreviousActivity();
-	}
+    public BookView bookView;
 
-	@Override
-	protected void onDestroy() {
-		super.onDestroy();
-		this.finish();
-		this.unregisterReceiver(this.broadcastReceiver);
-	}
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        foreground = true;
+        setContentView(R.layout.activity_anreader);
+        BookView bookView = (BookView) findViewById(R.id.bookView);
+        bookView.setPath(Constants.CACHE_PAHT);
+        bookView.initBook();
+        bookView.openShelf();
+        Intent intent = getIntent();
+        bookView.openBookFromFileExplorer(intent);
+    }
 
-	protected void finishPreviousActivity() {
-		Intent intent = new Intent();
-		intent.setAction("com.anfengde.ExitApp");
-		this.sendBroadcast(intent);
-	}
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.an_reader, menu);
+        return true;
+    }
 
-	@Override
-	protected void onPause() {
-		super.onPause();
-		foreground = false;
-	}
+    @Override
+    public void onResume() {
+        super.onResume();
+        foreground = true;
+        IntentFilter filter = new IntentFilter();
+        filter.addAction("com.anfengde.ExitApp");
+        this.registerReceiver(this.broadcastReceiver, filter);
+        finishPreviousActivity();
+    }
 
-	@Override
-	protected void onStop() {
-		super.onStop();
-		foreground = false;
-	}
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        this.finish();
+        this.unregisterReceiver(this.broadcastReceiver);
+    }
+
+    protected void finishPreviousActivity() {
+        Intent intent = new Intent();
+        intent.setAction("com.anfengde.ExitApp");
+        this.sendBroadcast(intent);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        foreground = false;
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        foreground = false;
+    }
 
 }
